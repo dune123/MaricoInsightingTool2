530b6705c36a94ea12ef2375c3130b6391308bff
sraj39273@gmail.com <sraj39273@gmail.com>
2025-10-27 16:51:55 +0530

    27 Oct changes

--- client/src/auth/msalConfig.ts
+++ client/src/auth/msalConfig.ts
@@ -19,35 +19,36 @@ export const createMsalConfig = (): Configuration => {
       redirectUri: import.meta.env.VITE_AZURE_REDIRECT_URI || currentOrigin,
       postLogoutRedirectUri: import.meta.env.VITE_AZURE_POST_LOGOUT_REDIRECT_URI || currentOrigin,
     },
-  cache: {
-    cacheLocation: 'sessionStorage', // This configures where your cache will be stored
-    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-  },
-  system: {
-    loggerOptions: {
-      loggerCallback: (level, message, containsPii) => {
-        if (containsPii) {
-          return;
-        }
-        switch (level) {
-          case 0: // LogLevel.Error
-            console.error(message);
+    cache: {
+      cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback: (level, message, containsPii) => {
+          if (containsPii) {
             return;
-          case 1: // LogLevel.Warning
-            console.warn(message);
-            return;
-          case 2: // LogLevel.Info
-            console.info(message);
-            return;
-          case 3: // LogLevel.Verbose
-            console.debug(message);
-            return;
-          default:
-            return;
-        }
+          }
+          switch (level) {
+            case 0: // LogLevel.Error
+              console.error(message);
+              return;
+            case 1: // LogLevel.Warning
+              console.warn(message);
+              return;
+            case 2: // LogLevel.Info
+              console.info(message);
+              return;
+            case 3: // LogLevel.Verbose
+              console.debug(message);
+              return;
+            default:
+              return;
+          }
+        },
       },
     },
-  },
+  };
 };
 
 // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
